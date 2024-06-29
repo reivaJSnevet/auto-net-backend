4e41e4731b1d4d7530915252e37e509d9f0cc03f
DyllanRuiz <dyllan.ruiz.vasquez@est.una.ac.cr>
2024-06-29 15:51:12 -0600

    chore: Update product module and related files

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -6,14 +6,23 @@ import {
   Delete,
   Body,
   Param,
-  ConflictException,
   NotFoundException,
+  BadRequestException,
+  ConflictException,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  UseFilters,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product-dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from 'src/schemas/product.shema';
+import { isValidObjectId } from 'mongoose';
 
+
+
+ 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
@@ -25,28 +34,48 @@ export class ProductController {
 
   @Get(':id')
   async getProductById(@Param('id') id: string): Promise<Product> {
-    const product = this.productService.getProductById(id);
+    if (!isValidObjectId(id)) {
+      throw new BadRequestException('No se encontro el producto con el ID ${id}');
+    }
+    const product = await this.productService.getProductById(id);
     if (!product) {
-      throw new NotFoundException('Product not found');
+      throw new NotFoundException('Producto no encontrado');
     }
     return product;
   }
 
   @Post()
+
+  //upgrade
   async createProduct(@Body() data: CreateProductDto): Promise<Product> {
-    return this.productService.createProduct(data);
+    try {
+      return this.productService.createProduct(data);
+    } catch (error) {
+      throw new BadRequestException("Error al crear el producto", error);
   }
+}
 
   @Put(':id')
   async updateProduct(
     @Param('id') id: string,
     @Body() data: UpdateProductDto,
   ): Promise<Product> {
-    return this.productService.updateProduct(id, data);
+    if (!isValidObjectId(id)) {
+      throw new BadRequestException('No se encontro el producto con el ID ${id}');
+    }
+    const product = await this.productService.updateProduct(id, data);
+    if (!product) {
+      throw new NotFoundException('Producto no encontrado');
+    }
+    return product;
   }
 
   @Delete(':id')
+  @HttpCode(204)
   async deleteProduct(@Param('id') id: string) {
-    return this.productService.deleteProduct(id);
+   const product = await this.productService.deleteProduct(id);
+    if (!product) {
+      throw new NotFoundException('Producto no encontrado');
+    }
   }
 }
